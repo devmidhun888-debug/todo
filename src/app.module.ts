bd1c1e7ba38d50f0769f1c3109bbe62715ec2cad
devmidhun888-debug <devmidhun888@gmail.com>
2025-09-20 19:22:02 +0530

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { TodoModule } from './todo';
+@Module({
+  imports: [
+     MongooseModule.forRoot('mongodb://127.0.0.1:27017/todo_app'),
+    AuthModule,
+    TodoModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
